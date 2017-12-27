bb9d45418096f8785133cff66f4ccee178d2bfcd
Youlich <jutatibouet@yahoo.fr>
2017-12-27 14:58:48 +0100

    notion d'heritage appel de la fonction plus simple
    Signed-off-by: Youlich <jutatibouet@yahoo.fr>

--- Heritage/Heritage.php
+++ Heritage/Heritage.php
@@ -3,6 +3,6 @@
 require 'Papa.php'; /**classe parente toujours en premier*/
 require 'Enfant.php';
 
-$enfant = new Enfant; /** création d'un objet enfant de la classe enfant */
-echo $enfant->herite(); /**fonction public de la classe enfant car l'autre fonction
- * de la classe parent était protected */
+
+echo (new Enfant)->herite();
+/** remplace $enfant = new Enfant; avec echo $enfant->herite();*/
